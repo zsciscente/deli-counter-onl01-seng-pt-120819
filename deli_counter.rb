7dd0b70df206ff979f753ea8d08a4dcb44ed81e0
Zachary Sciscente <zsciscente@gmail.com>
2019-12-21 02:38:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def now_serving(katz_deli)
  if  katz_deli.size==0
     puts "There is nobody waiting to be served!"
 else
-  katz_deli.each.first do |name|
+  katz_deli.each do |name|
     puts "Currently serving #{name}."
     
   end
